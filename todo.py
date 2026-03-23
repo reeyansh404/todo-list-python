284c30b00503082cca87175f0d74e8fd118bf6bf
reeyansh404 <121353254+reeyansh404@users.noreply.github.com>
2026-03-23 22:28:29 +1100

    initial project setup

---
+++ todo.py
@@ -0,0 +1,6 @@
+task = []
+print("*******Things To Do List*******")
+print("\n-------- Welcome to your To Do List --------\n")
+print("1. Add a task")
+print("2. View Tasks")
+print("3. Delete a task")
